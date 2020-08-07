00997aa5923e4f254d198c12a6508ab3c8e911f9
peawarrior <jacksonchen171@gmail.com>
2020-08-07 15:05:11 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(games) {
+  let answer = games.find(game => (game.result === "W"));
+  if (answer) {
+    return answer.year
+  } else {
+    return undefined
+  }
+}
