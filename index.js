27aca2857b3afc14af72c1ec512fdc7d1976a0ad
AlveeM <alvee.akand@outlook.com>
2020-08-08 14:09:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(records) {
+  const match = records.find(record => record.result == "W")
+  if (match != undefined) {
+    return match.year;
+  } else {
+    return undefined
+  }
 }
